f567573dca52c59058f30616873964b70526975b
lUCI <lusindisontanjana@gmail.com>
2022-11-11 11:46:19 +0200

    Complete the categories section

--- frontend/src/UI/Categories.jsx
+++ frontend/src/UI/Categories.jsx
@@ -1,29 +1,50 @@
+import { Link } from 'react-router-dom';
 import './Categories.scss';
 import Arrow from '../assets/icon-arrow-right.svg';
+import Headphones from '../assets/image-headphones.png';
+import Speakers from '../assets/image-speakers.png';
+import Earphones from '../assets/image-earphones.png';
+
+
+
 
 const Categories = () => {
   return (
     <section className="categories container">
       <div className="category">
-        <div className="category--image"></div>
+        <div className="category--image">
+          <img src={Headphones} alt="" />
+        </div>
         <div className="category--text">
           <h3>Headphones</h3>
-          <p><span>Shop</span> <img src={Arrow} alt="" /></p>
+          <Link to='/headphones'>
+            <p><span>Shop</span><img src={Arrow} alt="" /></p>
+
+          </Link>
         </div>
       </div>
       <div className="category">
-        <div className="category--image"></div>
-        <div className="category--text">
-          <h3>Speakers</h3>
-          <p><span>Shop</span> <img src={Arrow} alt="" /></p>
+        <div className="category--image">
+          <img src={Speakers} alt="" />
 
         </div>
+        <div className="category--text">
+          <h3>Speakers</h3>
+          <Link to='/speakers'>
+            <p><span>Shop</span><img src={Arrow} alt="" /></p>
+          </Link>
+        </div>
       </div>
       <div className="category">
-        <div className="category--image"></div>
+        <div className="category--image">
+          <img src={Earphones} alt="" />
+
+        </div>
         <div className="category--text">
           <h3>Earphones</h3>
-          <p><span>Shop</span> <img src={Arrow} alt="" /></p>
+          <Link to='/earphones'>
+            <p><span>Shop</span><img src={Arrow} alt="" /></p>
+          </Link>
         </div>
       </div>
     </section>
