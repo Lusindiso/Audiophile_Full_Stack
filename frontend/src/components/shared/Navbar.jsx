f8d4f8cab94eef1321dbf573d15414e1c9bef2d5
lUCI <lusindisontanjana@gmail.com>
2022-11-13 18:25:39 +0200

    Add hero section

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,12 +1,18 @@
+import { Link } from 'react-router-dom';
+
 import Nav from './Nav';
 import './Navbar.scss';
+import Logo from '../../assets/logo.svg';
+
 
 const Navbar = () => {
 	return (
 		<nav className="navbar">
 			<div className="container">
 				<div className="navbar--logo">
-					audiophile
+					<Link to='/'>
+						<img src={Logo} alt="" />
+					</Link>
 				</div>
 				<Nav />
 				<div className="navbar--right">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import About from "../UI/About";
 import Categories from "../UI/Categories";
+import Hero from "../UI/Hero";
 
 const Home = () => {
 	return (
 		<>
+			<Hero />
 			<Categories />
 			<About />
 		</>
